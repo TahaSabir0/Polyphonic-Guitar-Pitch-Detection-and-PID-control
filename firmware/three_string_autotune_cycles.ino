9ab0f4317ab6d01558842749d26f8ee3eb577e02
Taha Sabir <sabita01@gettysburg.edu>
2026-01-07 16:10:03 +0100

    file cleaning

--- firmware/three_string_autotune_cycles.ino
+++
@@ -1,6 +0,0 @@
-/*********************************************************************
- * Purpose: Automatic tuning for three strings in cycles.
- * Board: Arduino Uno R4
- * Description: This sketch will automatically tune three strings by
- * cycling through them and making small adjustments in each pass.
- *********************************************************************/
